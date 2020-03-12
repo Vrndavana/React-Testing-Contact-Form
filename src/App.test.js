e163320e72bf5403946ea3f288e4351fe33532a3
Vrndavana <Vrmdavama.th@gmail.com>
2020-03-12 14:47:09 -0700

    Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText, getByTitle } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+
+
+test("is message there?", () => {
+  const {getByText} =  render(<App/>);
+  getByText(/message/i);
+//  expect(Message).toBeInDocument();
+});
+
+
+
+test("First Name?", () => {
+  const {getByText} =  render(<App/>);
+  getByText(/First Name/i);
+});
+
+
+// test("Submit?", () => {
+//   const {getByTitle} =  render(<App/>);
+//   getByTitle(/Submit/i);
+// });
+
+
+
